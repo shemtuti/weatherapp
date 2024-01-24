5b95eb30ad1351952a3383ed6953fc0e2e2b6c33
shemtuti <shemtuti@gmail.com>
2024-01-24 21:50:39 +0300

    Ktlint and spotless setup

---
+++ .gradle/8.2/dependencies-accessors/d47d7142f8b5ecfef4a8bfde4f90adf9d1be413b/sources/org/gradle/accessors/dm/LibrariesForLibsInPluginsBlock.java
@@ -0,0 +1,130 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import org.gradle.api.internal.attributes.ImmutableAttributesFactory;
+import org.gradle.api.internal.artifacts.dsl.CapabilityNotationParser;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the `libs` extension.
+ */
+@NonNullApi
+public class LibrariesForLibsInPluginsBlock extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config, attributesFactory, capabilityNotationParser);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibsInPluginsBlock(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) {
+        super(config, providers, objects, attributesFactory, capabilityNotationParser);
+    }
+
+    /**
+     * Returns the group of versions at versions
+     */
+    public VersionAccessors getVersions() {
+        return vaccForVersionAccessors;
+    }
+
+    /**
+     * Returns the group of bundles at bundles
+     * @deprecated Will be removed in Gradle 9.0.
+     */
+    @Deprecated
+    public BundleAccessors getBundles() {
+        org.gradle.internal.deprecation.DeprecationLogger.deprecateBehaviour("Accessing libraries or bundles from version catalogs in the plugins block.").withAdvice("Only use versions or plugins from catalogs in the plugins block.").willBeRemovedInGradle9().withUpgradeGuideSection(8, "kotlin_dsl_deprecated_catalogs_plugins_block").nagUser();
+        return baccForBundleAccessors;
+    }
+
+    /**
+     * Returns the group of plugins at plugins
+     */
+    public PluginAccessors getPlugins() {
+        return paccForPluginAccessors;
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Returns the version associated to this alias: ktlint (1.0.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getKtlint() { return getVersion("ktlint"); }
+
+            /**
+             * Returns the version associated to this alias: ktlintGradle (11.6.1)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getKtlintGradle() { return getVersion("ktlintGradle"); }
+
+            /**
+             * Returns the version associated to this alias: spotless (6.23.3)
+             * If the version is a rich version and that its not expressible as a
+             * single version string, then an empty string is returned.
+             * This version was declared in catalog libs.versions.toml
+             */
+            public Provider<String> getSpotless() { return getVersion("spotless"); }
+
+    }
+
+    /**
+     * @deprecated Will be removed in Gradle 9.0.
+     */
+    @Deprecated
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config, ImmutableAttributesFactory attributesFactory, CapabilityNotationParser capabilityNotationParser) { super(objects, providers, config, attributesFactory, capabilityNotationParser); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+        private final KtlintPluginAccessors paccForKtlintPluginAccessors = new KtlintPluginAccessors(providers, config);
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Creates a plugin provider for spotless to the plugin id 'com.diffplug.spotless'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getSpotless() { return createPlugin("spotless"); }
+
+        /**
+         * Returns the group of plugins at plugins.ktlint
+         */
+        public KtlintPluginAccessors getKtlint() {
+            return paccForKtlintPluginAccessors;
+        }
+
+    }
+
+    public static class KtlintPluginAccessors extends PluginFactory {
+
+        public KtlintPluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+            /**
+             * Creates a plugin provider for ktlint.gradle to the plugin id 'org.jlleitschuh.gradle.ktlint'
+             * This plugin was declared in catalog libs.versions.toml
+             */
+            public Provider<PluginDependency> getGradle() { return createPlugin("ktlint.gradle"); }
+
+    }
+
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,67 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.gradle.spotless.SpotlessExtensionPredeclare
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.libsDirectory
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.diffplug.spotless") version "6.25.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0" apply false
+}
+
+apply(from = "buildscripts/git-hooks.gradle")
+
+allprojects {
+
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-opt-in=kotlin.RequiresOptIn",
+                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                "-opt-in=kotlinx.coroutines.FlowPreview",
+                "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+                "-opt-in=androidx.compose.foundation.ExperimentalFoundationApi",
+            )
+        }
+    }
+
+    val ktlintVersion = "1.0.1"
+
+    apply<org.jlleitschuh.gradle.ktlint.KtlintPlugin>()
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set(ktlintVersion)
+        android.set(true)
+        verbose.set(true)
+    }
+
+    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+    // Spotless Configuration
+    val configureSpotless: SpotlessExtension.() -> Unit = {
+        kotlin {
+            ktlint(ktlintVersion)
+            target("**/*.kt")
+            targetExclude("**/build/**/*.kt")
+        }
+        kotlinGradle {
+            ktlint(ktlintVersion)
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+        }
+        format("xml") {
+            target("**/*.xml")
+            targetExclude("**/build/**/*.xml")
+        }
+    }
+
+    if (project === rootProject) {
+        extensions.configure<SpotlessExtension> { predeclareDeps() }
+        extensions.configure<SpotlessExtensionPredeclare>(configureSpotless)
+    } else {
+        extensions.configure(configureSpotless)
+    }
 }
